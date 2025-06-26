a90b236b2d6abfb99c0e2e133aecdbd07a389949
317083-Matteo <317083@studenti.unimore.it>
2025-06-26 14:55:15 +0200

    mettere doxygen

--- compito.cpp
+++ compito.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 using namespace std;
 
-
+/**
+ * @author matteo gramazio 317083
+ * @param a intero
+ * @return somma a+2
+ */
 int func(int a) {
 cout<<"a+2="<<a+2;
 return a+2;
